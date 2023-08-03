90d2eb50590708a888ba5fe33638bd533d987eb4
Michal <hello@michalk.nl>
2023-08-03 12:31:06 +0200

    worked on the database and the dsn sentry

--- src/handler/Sentry.ts
+++ src/handler/Sentry.ts
@@ -1,7 +1,9 @@
+import { prismaClient } from "./Prisma";
+
 const Sentry = require("@sentry/node");
 
 Sentry.init({
-    dsn: "https://92854449ec4836bd22148f79d9af97ae@o1017660.ingest.sentry.io/4505636783980544",
+    dsn: ,
 
     tracesSampleRate: 1.0,
 });
